0b7acbac593b0be23c22aede0d2a00b4c1f39800
sonyerin ksu <yerin04@ks.ac.kr>
2025-04-07 03:58:38 +0000

    과제

--- myif.py
+++ myif.py
@@ -27,39 +27,52 @@ def print_rabbit():
     for line in rabbit:
         print(line)
 
-def play_game():
-    print("그림 출력 프로그램")
-    print("=====================")
-    print("1. 고양이")
-    print("2. 곰돌이")
-    print("3. 토끼")
-    print("=====================")
-    n = int(input("선택: "))
-   
-    # 만약에 1을 입력하면 1번에 해당하는 캐릭터 출력
-    if n == 1:
-        print("고양이")
-        print_cat()
-    # 2를 입력하면 2번 캐릭터 출력
-    elif n == 2:
-        print("곰돌이")
-        print_bear()
-    # 3을 입력하면 3번 캐릭터 출력
-    elif n == 3:
-        print("토끼")
-        print_rabbit()
-    # 잘못 입력하면 잘못 입력했다고 출력
+# 이 함수를 수정하여 두 개의 프로그램이 될 수 있게 만드시오.
+def play_game(play):
+    if play == "0":
+        r =  print("프로그램이 종료됩니니다.")
     else:
-        print("해당 숫자에 대한 동물이 없습니다")
+        r
+
+    return r
+
+r = play_game(play)
+print("그림 출력 프로그램")
+print("=====================")
+print("1. 고양이")
+print("2. 곰돌이")
+print("3. 토끼")
+print("=====================")
+n = int(input("선택: "))
+
+# 만약에 1을 입력하면 1번에 해당하는 캐릭터 출력
+if n == 1:
+    print("고양이")
+    print_cat()
+# 2를 입력하면 2번 캐릭터 출력
+elif n == 2:
+    print("곰돌이")
+    print_bear()
+# 3을 입력하면 3번 캐릭터 출력
+elif n == 3:
+    print("토끼")
+    print_rabbit()
+# 잘못 입력하면 잘못 입력했다고 출력
+else:
+    print("해당 숫자에 대한 동물이 없습니다")
 
 # 동물 그림 출력 프로그램이 총 5번 반복 실행될 수 있게 만드시오.
+print("5번 출력 프로그램 시작")
 for i in range(5):
-   play_game()
+   play_game(play)
+
+print("5번 출력 프로그램 종료")
 
 # 위 프로그램을 완성한한 사람은 프로그램이 계속(무한) 반복하게 하고
 # 만약에 0을 입력하면 종료되는 프로그램을 만드시오.
+print("0을 입력하면 종료 프로그램 시작")
 while True: # 무한반복(계속 참)
-   play_game()
+   play_game(play)
 
-
+print("0을 입력하면 종료 프로그램 종료")
 
